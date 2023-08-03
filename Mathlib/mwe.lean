c585f380bd9cb608d19068a3ceda0a1b40149b10
Xavier-François Roblot <46200072+xroblot@users.noreply.github.com>
2023-08-03 10:00:22 +0900

    wip

--- Mathlib/mwe.lean
+++ Mathlib/mwe.lean
@@ -7,13 +7,19 @@ variable (K : Type _) [Field K] (n : ℕ)
 
 def torsion : Subgroup (𝓞 K)ˣ := CommGroup.torsion (𝓞 K)ˣ
 
+set_option trace.profiler true in
+set_option trace.Meta.synthInstance true in
+set_option trace.Meta.isDefEq true in
 set_option maxHeartbeats 500000 in
 set_option synthInstance.maxHeartbeats 100000 in
 def Basis_additive : Basis (Fin n) ℤ (Additive ((𝓞 K)ˣ ⧸ (torsion K))) := sorry
 
+set_option trace.profiler true in
+set_option trace.Meta.synthInstance true in
+set_option trace.Meta.isDefEq true in
 set_option maxHeartbeats 800000 in
 set_option synthInstance.maxHeartbeats 100000 in
 example {ι : Type _} [Fintype ι] (f : ι → (𝓞 K)ˣ) :
-  QuotientGroup.mk' (torsion K) (∏ i, (f i)) =
-    ∏ i, QuotientGroup.mk' (torsion K) (f i) := by
-  refine map_prod (QuotientGroup.mk' (torsion K)) _ _
+    ((∏ i, (f i) : (𝓞 K)ˣ) : (𝓞 K)ˣ ⧸ (torsion K)) = ∏ i, ↑(f i) := by
+  simp_rw [← QuotientGroup.mk'_apply]
+  convert map_prod (QuotientGroup.mk' (torsion K)) f Finset.univ
